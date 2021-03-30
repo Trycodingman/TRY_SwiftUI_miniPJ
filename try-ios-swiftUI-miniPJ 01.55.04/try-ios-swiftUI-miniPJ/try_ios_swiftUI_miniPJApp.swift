7a02cf40e46c462e50300be58363ff79049e845d
trycodingman <trycodingman@gmail.com>
2021-03-31 01:56:29 +0900

    [20210331][TryCodingman] ■ Complete ■ ■ SwiftUI 공부를 위한 기반 셋팅 완료
    □ In Process □
    □
    
    ※ Issue ※
    ※

---
+++ try-ios-swiftUI-miniPJ 01.55.04/try-ios-swiftUI-miniPJ/try_ios_swiftUI_miniPJApp.swift
@@ -0,0 +1,17 @@
+//
+//  try_ios_swiftUI_miniPJApp.swift
+//  try-ios-swiftUI-miniPJ
+//
+//  Created by trycodingman on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct try_ios_swiftUI_miniPJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
